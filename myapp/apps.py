6b63562b05fa0ab6b8cdb15c5df16b8165831d25
kabekum <98370249+kabekum@users.noreply.github.com>
2025-12-24 10:59:51 +0300

    Change app verbose name from 'Task Manager' to 'myapp'

--- myapp/apps.py
+++ myapp/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
-    verbose_name = "Task Manager" # change the app name in admin panel
+    verbose_name = "myapp" # change the app name in admin panel
     
     def ready(self):
         import myapp.signals  # register signals
